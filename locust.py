6f0f5fdd9f61bae37a5d2fca090b22cc53a51953
Amit Shah <amitshah0t7@gmail.com>
2020-09-11 19:43:16 -0400

    add lifo queue to simulate parallel getrawtransaction call

--- locust.py
+++ locust.py
@@ -2,22 +2,30 @@ import requests
 import time
 import auth
 from locust import SequentialTaskSet,HttpUser,HttpLocust, task, between,events
+import threading,queue
 
 
 user = auth.user
 password= auth.password
 url = auth.url
 
-def tx_factory(tx_hashes):
-  tx_hash = tx_hashes.pop()
-  pass
+def tx_factory(locust): 
+    tx = tx_hashes.get()
+    print("Create Tx TxLocust:"+tx)
+    locust.client.post(url=url,
+            data='{"jsonrpc": "1.0", "id":"load_tester", "method": "getrawtransaction", "params": ["'+ tx+'"] }',
+            auth=(user,password),
+            #headers={"authorization": "bearer " + 'token'},
+            name="/tx/"+str(tx))
+    tx_hashes.task_done()
 
 block_num = 464000
+#this shared object is a bottleneck, we are better off getting a list of transactions (millions) loading the and processing them in a single locust rather then coordinate 2 locusts
+tx_hashes = queue.LifoQueue()
 
 class SequenceOfTasksSpec1(SequentialTaskSet):
 
     blockhash = ''
-    tx_hashes = [1,2,3,4]
 
     @task
     def get_blockhash(self):
@@ -37,14 +45,20 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
     def get_block(self):
         resp = self.client.post(
             url=url,
-            data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'+self.blockhash+'",2] }',
+            data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'+self.blockhash+'",1] }',
             auth=(user,password),
             #headers={"authorization": "bearer " + 'token'},
             name="/getblock/"+self.blockhash,
         )
-        self.tx_hashes = [1,2,3,4]
+        json_response_dict = resp.json()
+        global tx_hashes
+        print("BEFORE:"+str(tx_hashes.qsize()))
+        for tx in json_response_dict['result']['tx']:
+          tx_hashes.put(tx)
+        print("AFTER:"+str(tx_hashes.qsize()))
         pass
         #TODO: get transaction hashs and set tx_hash = []
+
 
     #tasks = [tx_factory(tx_hashes)]
 
@@ -58,10 +72,18 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
       print("A new test is ending")
       pass
 
+#TXLocust 20 times larger workpool then block
+class TxLocust(HttpUser):
+  weight = 1
+  tasks = [tx_factory]
+  min_wait = 10
+  max_wait = 20
+
 class MyLocust(HttpUser):
-    tasks = [SequenceOfTasksSpec1]
-    min_wait = 10
-    max_wait = 50
+  weight=1
+  tasks = [SequenceOfTasksSpec1]
+  min_wait = 10
+  max_wait = 50
 
 
 # def tx_factory(path):
