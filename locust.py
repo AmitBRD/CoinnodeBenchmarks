d0f1babd3e824f0c6419b989d0d638c077810f44
Amit Shah <amitshah0t7@gmail.com>
2020-09-11 20:22:43 -0400

    correct grouping for stats by updating name= field

--- locust.py
+++ locust.py
@@ -9,6 +9,8 @@ user = auth.user
 password= auth.password
 url = auth.url
 
+TX_BLOCK_WORKER_RATIO = 4
+
 def tx_factory(locust): 
     tx = tx_hashes.get()
     print("Create Tx TxLocust:"+tx)
@@ -16,7 +18,7 @@ def tx_factory(locust):
             data='{"jsonrpc": "1.0", "id":"load_tester", "method": "getrawtransaction", "params": ["'+ tx+'"] }',
             auth=(user,password),
             #headers={"authorization": "bearer " + 'token'},
-            name="/tx/"+str(tx))
+            name="/tx")
     tx_hashes.task_done()
 
 block_num = 464000
@@ -36,7 +38,7 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
             data='{"jsonrpc": "1.0", "id":"load_tester", "method": "getblockhash", "params": ['+ str(block_num)+'] }',
             auth=(user,password),
             #headers={"authorization": "bearer " + 'token'},
-            name="/block/"+str(block_num))
+            name="/block")
       json_response_dict = resp.json()
       self.blockhash = json_response_dict['result']
       pass
@@ -45,10 +47,34 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
     def get_block(self):
         resp = self.client.post(
             url=url,
+            data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'+self.blockhash+'",2] }',
+            auth=(user,password),
+            #headers={"authorization": "bearer " + 'token'},
+            name="/getblock",
+        )
+        json_response_dict = resp.json()
+        pass
+
+    @events.test_start.add_listener
+    def on_test_start(**kwargs):
+      print("A new test is starting")
+      pass
+
+    @events.test_stop.add_listener
+    def on_test_stop(**kwargs):
+      print("A new test is ending")
+      pass
+
+class SequenceOfTasksSpec2(SequenceOfTasksSpec1):
+
+    @task
+    def get_block(self):
+        resp = self.client.post(
+            url=url,
             data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["'+self.blockhash+'",1] }',
             auth=(user,password),
             #headers={"authorization": "bearer " + 'token'},
-            name="/getblock/"+self.blockhash,
+            name="/getblock",
         )
         json_response_dict = resp.json()
         global tx_hashes
@@ -57,10 +83,7 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
           tx_hashes.put(tx)
         print("AFTER:"+str(tx_hashes.qsize()))
         pass
-        #TODO: get transaction hashs and set tx_hash = []
-
-
-    #tasks = [tx_factory(tx_hashes)]
+       
 
     @events.test_start.add_listener
     def on_test_start(**kwargs):
@@ -73,84 +96,27 @@ class SequenceOfTasksSpec1(SequentialTaskSet):
       pass
 
 #TXLocust 20 times larger workpool then block
+
 class TxLocust(HttpUser):
-  weight = 1
+  weight = TX_BLOCK_WORKER_RATIO
   tasks = [tx_factory]
   min_wait = 10
-  max_wait = 20
+  max_wait = 15
 
-class MyLocust(HttpUser):
+class BlockLocust(HttpUser):
   weight=1
-  tasks = [SequenceOfTasksSpec1]
+  tasks = [SequenceOfTasksSpec2] 
   min_wait = 10
   max_wait = 50
 
 
-# def tx_factory(path):
-# 	def _locust(locust):
-# 		locust.client.post(
-#             url="http://167.172.193.236:8889",
-#             data='{"jsonrpc": "1.0", "id":"curltest", "method": "getrawtransaction", "params": ["000000000000009a753dd0a038673fa672d9170d62a794244f9647550e37b701"] }',
-#             auth=('michael','jEFnnKPhO93yBYx3C0tGUsUfqz_G3_BKBdM6-CGf1lY='),
-#             #headers={"authorization": "bearer " + 'token'},
-#             name="/getblock",
-#         )
+#If you want to run on old spec benchmark then comment above 2 and enable below
 
-# class SequenceOfTasksSpec2(SequentialTaskSet):
-
-#     @task
-#     def get_blockhash(self):
-#     	resp = self.client.post(
-#             url="http://167.172.193.236:8889",
-#             data='{"jsonrpc": "1.0", "id":"load_tester", "method": "getblockhash", "params": ["000000000000009a753dd0a038673fa672d9170d62a794244f9647550e37b701"] }',
-#             auth=('michael','jEFnnKPhO93yBYx3C0tGUsUfqz_G3_BKBdM6-CGf1lY='),
-#             #headers={"authorization": "bearer " + 'token'},
-#             name="/getblockhash",
-#         )
-#         pass
-#  	@task
-#     def get_block(self):
-#         resp = self.client.post(
-#             url="http://167.172.193.236:8889",
-#             data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["000000000000009a753dd0a038673fa672d9170d62a794244f9647550e37b701"] }',
-#             auth=('michael','jEFnnKPhO93yBYx3C0tGUsUfqz_G3_BKBdM6-CGf1lY='),
-#             #headers={"authorization": "bearer " + 'token'},
-#             name="/getblock",
-#         )
-
-#     @task
-#     def get_transaction(self):
-#         pass
+# class BlockLocust(HttpUser)
+#   weight = 1
+#   tasks = [SequenceOfTasksSpec1]
+#   min_wait = 10
+#   max_wait = 20
 
 
-# class QuickstartUser(HttpUser):
-#     wait_time = between(1, 2)
-
-   
-
-#     @task
-#     def get_blockhash(self):
-#         resp = self.client.post(
-#             url="http://167.172.193.236:8889",
-#             data='{"jsonrpc": "1.0", "id":"curltest", "method": "getblock", "params": ["000000000000009a753dd0a038673fa672d9170d62a794244f9647550e37b701"] }',
-#             auth=('michael','jEFnnKPhO93yBYx3C0tGUsUfqz_G3_BKBdM6-CGf1lY='),
-#             #headers={"authorization": "bearer " + 'token'},
-#             name="/getblock",
-#         )
-
-
-    # @task
-    # def index_page(self):
-    #     self.client.get("/hello")
-    #     self.client.get("/world")
-
-    # @task(3)
-    # def view_item(self):
-    #     for item_id in range(10):
-    #         self.client.get(f"/item?id={item_id}", name="/item")
-    #         time.sleep(1)
-
-    # def on_start(self):
-    #     self.client.post("/login", json={"username":"foo", "password":"bar"})
-	#flag = False
 
